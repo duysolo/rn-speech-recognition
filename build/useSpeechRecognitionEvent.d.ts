48b879d4d75a09b6f65275fb542d1f1c2b4e2e95
Justin Phan <duypt.dev@gmail.com>
2026-04-03 20:59:56 +0700

    Remove build from git, restore .gitignore

--- build/useSpeechRecognitionEvent.d.ts
+++
@@ -1,3 +0,0 @@
-import type { RNSpeechRecognitionNativeEventMap } from "./RNSpeechRecognitionModule.types";
-export declare function useSpeechRecognitionEvent<K extends keyof RNSpeechRecognitionNativeEventMap>(eventName: K, listener: (event: RNSpeechRecognitionNativeEventMap[K]) => void): void;
-//# sourceMappingURL=useSpeechRecognitionEvent.d.ts.map
